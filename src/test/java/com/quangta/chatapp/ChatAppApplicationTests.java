6076049911489756f5e257b48bf82b63811bde55
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-05-11 21:56:33 +0700

    build: initialize server base

---
+++ src/test/java/com/quangta/chatapp/ChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quangta.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
